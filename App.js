87378231575aeaf5d6bc7ec4e0fa240397fa3f46
Uzodike Oguejiofor <reenydavidson@gmail.com>
2022-04-11 14:53:15 +0100

    integrating provider

--- App.js
+++ App.js
@@ -2,8 +2,6 @@ import React, {useEffect, useRef, useState} from 'react';
 import {NativeBaseProvider, extendTheme, StatusBar} from 'native-base';
 import 'react-native-gesture-handler';
 import DrawerNavigation from './navigation/DrawerNavigation';
-import HmsManager from '@100mslive/react-native-hms';
-
 import {HMSProvider} from './components/HmsContext';
 import {setupBuild} from './100ms/100ms';
 
@@ -17,8 +15,8 @@ const config = {
 export const theme = extendTheme({config});
 
 export default function App() {
-  const [hmsInstanceLoaded, setHmsInstanceLoaded] = useState(false);
   const hmsInstance = useRef(null);
+  const [hmsInstanceLoaded, setHmsInstanceLoaded] = useState(false);
 
   useEffect(() => {
     setupBuild().then(build => {
@@ -32,13 +30,15 @@ export default function App() {
   }
 
   return (
-    <NativeBaseProvider theme={theme}>
-      {hmsInstanceLoaded ? <DrawerNavigation /> : null}
-      <StatusBar
-        barStyle={theme.dark ? 'light-content' : 'dark-content'}
-        backgroundColor={theme.dark ? '#000' : '#fff'}
-      />
-    </NativeBaseProvider>
+    <HMSProvider value={hmsInstance.current}>
+      <NativeBaseProvider theme={theme}>
+        {hmsInstanceLoaded ? <DrawerNavigation /> : null}
+        <StatusBar
+          barStyle={theme.dark ? 'light-content' : 'dark-content'}
+          backgroundColor={theme.dark ? '#000' : '#fff'}
+        />
+      </NativeBaseProvider>
+    </HMSProvider>
   );
 }
 
